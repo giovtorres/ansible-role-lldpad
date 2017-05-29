5a112a8f9bc88fc958a93ee493e1dce0f361349d
Giovanni Torres <giovanni.torres@gmail.com>
2017-05-29 17:00:58 -0400

    Fallback to older testinfra without host fixture

--- tests/test_default.py
+++ tests/test_default.py
@@ -5,22 +5,22 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     '.molecule/ansible_inventory').get_hosts('all')
 
 
-def test_lldpad_service(host):
-    s = host.service("lldpad")
+def test_lldpad_service(Service):
+    s = Service("lldpad")
     assert s.is_enabled
     assert s.is_running
 
 
-def test_lldpad_adminstatus(host):
+def test_lldpad_adminstatus(Command, Sudo):
     cmd = "/usr/sbin/lldptool get-lldp -i eth0 adminStatus"
-    with host.sudo():
-        assert host.run_expect([0], cmd)
-        assert "adminStatus=rxtx" in host.check_output(cmd)
+    with Sudo():
+        assert Command.run_expect([0], cmd)
+        assert "adminStatus=rxtx" in Command.check_output(cmd)
 
 
 @pytest.mark.parametrize("tlvid", ["sysName", "portDesc"])
-def test_lldpad_get_tlvs(host, tlvid):
+def test_lldpad_get_tlvs(Command, Sudo, tlvid):
     cmd = "/usr/sbin/lldptool get-tlv -i eth0 -c enableTx -V " + tlvid
-    with host.sudo():
-        assert host.run_expect([0], cmd)
-        assert "enableTx=yes" in host.check_output(cmd)
+    with Sudo():
+        assert Command.run_expect([0], cmd)
+        assert "enableTx=yes" in Command.check_output(cmd)
